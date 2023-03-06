550994f028c812a91882dabfe32519fd26740671
dresan22 <andresat48@hotmail.com>
2023-03-06 18:32:45 -0400

    remove withCredentials attribute

--- src/components/uploader/Uploader.jsx
+++ src/components/uploader/Uploader.jsx
@@ -8,74 +8,74 @@ import useSettings from '../../store/Context';
 import './filepond.scss';
 
 export function Uploader() {
-    const [files, setFiles] = React.useState([]);
+ const [files, setFiles] = React.useState([]);
 
-    const myURL = 'https://api.cloudinary.com/v1_1/dresan22/image/upload';
+ const myURL = 'https://api.cloudinary.com/v1_1/dresan22/image/upload';
 
-    const {
-      originalImage,
-      setOriginalImage,
-      publicId,
-      setPublicId,
-      setUploadComplete,
-      uploadComplete,
-      setHeight,
-      setWidth,
-      setOriginalWidth,
-      setOriginalHeight,
-      imageURL,
-      setEditedImage,
-    } = useSettings();
+ const {
+   originalImage,
+   setOriginalImage,
+   publicId,
+   setPublicId,
+   setUploadComplete,
+   uploadComplete,
+   setHeight,
+   setWidth,
+   setOriginalWidth,
+   setOriginalHeight,
+   imageURL,
+   setEditedImage,
+ } = useSettings();
 
-  const handleOnLoad = (response) => {
-    console.log(`🚀 ~ response:`, response);
-    if (!response) return;
-    const result = JSON.parse(response);
-    console.log(`🚀 ~ result:`, result);
-    // console.log(`🚀 ~ responseJSON:`, responseJSON);
-    setOriginalImage(result.secure_url);
-    setEditedImage(result.secure_url);
-    setPublicId(result.public_id);
-    setHeight(result.height);
-    setWidth(result.width);
-    setOriginalWidth(result.width);
-    setOriginalHeight(result.height);
-    setUploadComplete(true);
-    return response.data;
-  };
+ const handleOnLoad = (response) => {
+   console.log(`🚀 ~ response:`, response);
+   if (!response) return;
+   const result = JSON.parse(response);
+   console.log(`🚀 ~ result:`, result);
+   // console.log(`🚀 ~ responseJSON:`, responseJSON);
+   setOriginalImage(result.secure_url);
+   setEditedImage(result.secure_url);
+   setPublicId(result.public_id);
+   setHeight(result.height);
+   setWidth(result.width);
+   setOriginalWidth(result.width);
+   setOriginalHeight(result.height);
+   setUploadComplete(true);
+   return response.data;
+ };
 
-  const moveToCorner = uploadComplete ? 'absolute top-5 right-5 z-10' : '';
+ const moveToCorner = uploadComplete ? 'absolute top-5 right-5 z-10' : '';
 
-  return (
-    <div className={`mx-auto ${moveToCorner}`}>
-      <FilePond
-        files={files}
-        onupdatefiles={setFiles}
-        maxFiles={1}
-        server={{
-          url: myURL,
-          process: {
-            method: 'POST',
-            withCredentials: false,
-            headers: { 'X-Requested-With': 'XMLHttpRequest' },
-            timeout: 7000,
-            onload: handleOnLoad,
-            onerror: (response) => response.data,
-            ondata: (formData) => {
-              formData.append('file', files[0].file);
-              formData.append('upload_preset', 'yoalpaao');
-              formData.append('api_key', '496498499269514');
-              formData.append('timestamp', (Date.now() / 1000) | 0);
-              return formData;
-            },
-          },
-        }}
-        name='files'
-        labelIdle='Drag & Drop your image or <span class="filepond--label-action">Browse</span>'
-        credits={false}
-      />
-    </div>
-  );
+ return (
+   <div className={`mx-auto ${moveToCorner}`}>
+     <FilePond
+       files={files}
+       onupdatefiles={setFiles}
+       maxFiles={1}
+       server={{
+         url: myURL,
+         process: {
+           method: 'POST',
+           // withCredentials: false,
+           headers: { 'X-Requested-With': 'XMLHttpRequest' },
+           timeout: 7000,
+           onload: handleOnLoad,
+           onerror: (response) => response.data,
+           ondata: (formData) => {
+             formData.append('file', files[0].file);
+             formData.append('upload_preset', 'yoalpaao');
+             formData.append('api_key', '496498499269514');
+             formData.append('timestamp', (Date.now() / 1000) | 0);
+             return formData;
+           },
+         },
+       }}
+       name='files'
+       labelIdle='Drag & Drop your image or <span class="filepond--label-action">Browse</span>'
+       credits={false}
+     />
+   </div>
+ );
 }
 
 
